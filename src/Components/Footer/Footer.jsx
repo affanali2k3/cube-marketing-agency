e8a3f5c486a3ae5c945133ab2d367e60514743b4
affanali2k3 <affanalikhanxx@gmail.com>
2023-09-24 03:29:47 +0500

    Changed footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,11 +3,20 @@ import './Footer.css'
 
 export const Footer = () => {
     return (
+        <div className="footer-main-div">
         <div className='footer'>
             <div>
                 <h3>Cube Media Marketing</h3>
                 <p>All Rights Reserved | 2023</p>
             </div>
-        </div>
+         
+        </div> <div className="copyright-para-div">  <p className='copyright-para'>
+The content, images, graphics, and other materials on this website are protected by copyright and other intellectual property laws. Any unauthorized use, reproduction, or distribution of the materials on this site is prohibited without the written consent of Cube Media Marketing.</p>
+<p>
+You may view and use the content on this website for your personal, non-commercial use only. You may not modify, distribute, transmit, display, perform, reproduce, publish, license, create derivative works from, transfer, or sell any content, information, software, products, or services obtained from this website.</p>
+<p>
+Cube Media Marketing is a registered trademark. All other trademarks, service marks, and trade names contained in this site are the properties of their respective owners.
+</p><p>
+For inquiries, please contact [support@cubemediamarketing.com].</p></div></div>
     )
 }
